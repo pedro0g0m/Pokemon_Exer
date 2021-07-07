86974cf27799695970d7d4b5df3aabfa5bdac094
pedro0g0m <pedrogmoreira94@gmail.com>
2021-07-07 16:32:32 +0100

    ...

--- Pokemon/Program.cs
+++ Pokemon/Program.cs
@@ -14,23 +14,30 @@ namespace Pokemon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nO Ash vai comecar a sua aventura!!\n\nUsando os pontos cardeais diz-lhe para onde ele deve ir :");
+            //Console.WriteLine("\nO Ash vai comecar a sua aventura!!\n\nUsando os pontos cardeais diz-lhe para onde ele deve ir :");
 
-            // 1 -> recolher dados
-            string valideMoves, moves = Console.ReadLine();
+            //// 1 -> recolher dados
+            //string valideMoves, moves = Console.ReadLine();
 
-            // 2 -> Validar
-            int steps = Move.Sort(moves, out valideMoves);
+            //// 2 -> Validar
+            //int steps = Move.Sort(moves, out valideMoves);
 
-            // 3 -> Criar o mapa |+| 4 -> Registar movimentos no mapa
-            int[,] map = Move.Catch(Move.CreateMap(steps), steps, valideMoves);
+            //// 3 -> Criar o mapa |+| 4 -> Registar movimentos no mapa
+            //int[,] map = Move.Catch(Move.CreateMap(steps), steps, valideMoves);
 
-            // 5-> Contar o numero de Pokemons apanhados
-            int pokedex = Move.Count(map);
+            //// 5-> Contar o numero de Pokemons apanhados
+            //int pokedex = Move.Count(map);
 
 
-            Console.WriteLine("O Ash deu {0} passos. E apanhou {1} pokemons.", steps, pokedex);
+            //Console.WriteLine("O Ash deu {0} passos. E apanhou {1} pokemons.", steps, pokedex);
+
+            //                          0          1
+            string[] arraysnomes = { "Diogo", "Pedro" };
+
+            Console.WriteLine(arraysnomes[1]);
+
 
         }
     }
 }
+[]
